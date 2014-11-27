ee0acc65d94cf1dff84237aea9039ebfa759717a
wddddw <im.wd95@gmail.com>
2014-11-27 18:12:55 +0800

    ver7

--- cparking.vol1.cpp
+++ cparking.vol1.cpp
@@ -24,15 +24,17 @@ struct Slot{
 	bool ispark;			//Recording the status of the Slot
 };
 
+int carnum=0;
 Object cars[MAXCAR];
 
-Slot CS1[100][20];
-Slot CS2[5][10];
-Slot CS3[10][10];
+Slot CS1[2000];
+Slot CS2[50];
+Slot CS3[100];
 
-bool ifFull(Slot **,int,int);	//to judge the Parking area if full
+bool isFull(Slot *p,int,int);	//to judge the Parking area if full
 int Random(int , int );		//to Generate a random number between [a,b]
-void RandomSlot(Slot **,int,int,int &,int &);
+void RandomSlot(Slot *p,int,int,int &,int &);
+int ParkIn(Object,Slot *p,int,int);
 
 
 
@@ -40,12 +42,9 @@ void RandomSlot(Slot **,int,int,int &,int &);
 
 
 
-bool isFull(Slot **p,int mx,int my){
-	for(int i=0;i<mx;i++){
-		for(int j=0;i<my;i++){
-			if(p[i][j].ispark==0) return 0;
-		}
-	}
+bool isFull(Slot *p,int mx,int my){
+	for(int i=0;i<mx*my;i++)
+		if(p[i].ispark==0) return 0;
 	return 1;
 }
 
@@ -67,12 +66,43 @@ int Random(int m, int n){
 	}
 }
 
-void RandomSlot(Slot ** p,int mx,int my,int & outx,int & outy){
+void RandomSlot(Slot *p,int mx,int my,int & outx,int & outy){
 	int x,y;
 	do{
 		x=Random(0,mx);
 		y=Random(0,my);
-	}while(p[x][y].ispark==1);
+	}while(p[my*x+y].ispark==1);
 	outx=x;
 	outy=y;
+}
+
+int ParkIn(Object temp,Slot *p,int mx,int my){
+	int x,y;
+	if(isFull(p,mx,my)) return -1;	//Error Code -1 Parking Area Full
+	else{
+		RandomSlot(p,mx,my,x,y);
+		temp.place[1]=x;
+		temp.place[2]=y;
+		cars[carnum]=temp;
+		p[my*x+y].pCAR=&cars[carnum];
+		if(carnum<=MAXCAR) carnum++;
+		return 1;					//Return Code 1 Parking Success!
+	}
+}
+
+
+int main(){
+	int t_num=0;
+	Object temp;
+	temp.kind='c';
+	temp.license="wwyydd";
+	temp.place[0]=1;
+	temp.rank=1;
+	//Test parking
+	while(ParkIn(temp,CS1,100,20)!=-1){
+		t_num++;
+		printf("%d\n",t_num);
+	}
+	printf("%d\n",t_num);
+	system("pause");
 }
