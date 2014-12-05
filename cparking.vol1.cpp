2cf93872863d8cc419d6ccdc01090b7cdb48b5d2
wddddw <im.wd95@gmail.com>
2014-12-06 00:03:44 +0800

    Account Exit Finished
    Fix Some Problem of Admin Control and Enter Control

--- cparking.vol1.cpp
+++ cparking.vol1.cpp
@@ -11,9 +11,7 @@
 #include <cstdlib>
 #include <ctime>
 using namespace std;
-#define MAXSIZE 8 //Max width of a car license#define MAXCAR 2150
-#define MAXCUST 10000
-struct Customer;
+#define MAXSIZE 8 //Max width of a car license#define MAXCAR 2150#define MAXCUST 10000struct Customer;
 
 struct Object {
 	Customer * cust;
@@ -41,12 +39,12 @@ Slot CS1[2000];
 Slot CS2[50];
 Slot CS3[100];
 
-const int CS1WIDTH=100;
-const int CS1LENGTH=20;
-const int CS2WIDTH=5;
-const int CS2LENGTH=10;
-const int CS3WIDTH=10;
-const int CS3LENGTH=10;
+const int CS1WIDTH = 100;
+const int CS1LENGTH = 20;
+const int CS2WIDTH = 5;
+const int CS2LENGTH = 10;
+const int CS3WIDTH = 10;
+const int CS3LENGTH = 10;
 
 string ADMINPASSWD = "0";
 string ENTPASSWD = "0";
@@ -55,15 +53,18 @@ string EXITPASSWD = "0";
 Customer CUST[MAXCUST];
 int custnum = 0;
 
-double CHARGETABLE[3];
-double TEACHERDISCOUNT;
+double CHARGETABLE[3] = { 10, 5, 5 };
+double TEACHERDISCOUNT = 0.8;
+double STUDENTDISCOUNT = 0.6;
 
+//Basic Function [Including Parking Algorithm]
 bool isFull(Slot *p, int, int); //to judge the Parking area if full
 int Random(int, int); //to Generate a random number between [a,b]
 void RandomSlot(Slot *p, int, int, int &, int &);
-int ParkIn(Object, Slot *p, int, int);
-int GoOut(Object, Slot, int, int);
+int ParkIn(Object &, Slot *p, int, int);
+int GoOut(Slot *, int, int, int, int);
 
+//Welcome
 void Welcome();
 
 //Functions of Admin Login Mod
@@ -85,7 +86,175 @@ void ChangeEntrancePasswd();
 //Functions of Exit Desk Mod
 void ExitLogin();
 void Exit();
+void ChangeExitPasswd();
+void LetObjectOut();
+void AccountOut();
+void GuestOut();
 
+/*
+ * Exit Desk Mod Start
+ */
+
+void ExitLogin() {
+	string temp;
+	for (int i = 0; i < 3; i++) {
+		system("cls");
+		printf("Please Input Exit Desk Password: ");
+		cin >> temp;
+		if (temp == EXITPASSWD) {
+			Exit();
+			return;
+		} else {
+			if (i == 1)
+				printf("Input Error, You have %d time to try!\n", 2 - i);
+			else {
+				if (i != 2)
+					printf("Input Error, You have %d times to try!\n", 2 - i);
+			}
+			system("pause");
+		}
+	}
+	return;
+}
+
+void Exit() {
+	char c;
+	while (1) {
+		system("cls");
+		printf("\t\t----Parking System(Exit Desk)---\n");
+		printf("\t1. Vehicle Out\n");
+		printf("\tp. Change Password\n");
+		printf("\tq. Enterance Exit\n");
+		printf("\tPlease Choose: ");
+		setbuf(stdin, NULL);
+		scanf("%c", &c);
+		switch (c) {
+		case '1':
+			LetObjectOut();
+			break;
+		case 'p':
+			ChangeExitPasswd();
+			break;
+		case 'q':
+			return;
+			break;
+		case 'Q':
+			return;
+			break;
+		default:
+			break;
+		}
+	}
+	system("pause");
+}
+
+void ChangeExitPasswd() {
+	string temp1, temp2;
+	system("cls");
+	printf("Please Input the New Password: \n");
+	cin >> temp1;
+	printf("Please Input Again: \n");
+	cin >> temp2;
+	if (temp1 == temp2)
+		EXITPASSWD = temp2;
+	else {
+		system("cls");
+		printf("Not Matched! \n");
+	}
+	return;
+}
+
+void LetObjectOut() {
+	char c = 0;
+	while (c != 'a' && c != 'b') {
+		system("cls");
+		setbuf(stdin, NULL);
+		printf("\t\t---You have account or Guest---\n");
+		printf("\ta. Account\n");
+		printf("\tb. Guest\n");
+		printf("Please Choose: ");
+		scanf("%c", &c);
+		switch (c) {
+		case 'a':
+			AccountOut();
+			return;
+			break;
+		case 'b':
+			GuestOut();
+			return;
+			break;
+		default:
+			printf("\nError!");
+			system("pause");
+			break;
+		}
+	}
+}
+
+void AccountOut() {
+	string acc;
+	double paypal;
+	int flag = -1;
+	while (flag == -1) {
+		system("cls");
+		setbuf(stdin, NULL);
+		printf("Please Enter Your Account Name:");
+		cin >> acc;
+		for (int i = 0; i < custnum; i++) {
+			if (acc == CUST[i].name) {
+				flag = i;
+				break;
+			}
+		}
+		if (flag == -1) {
+			printf("Account Not Found, Please reEnter");
+			system("pause");
+		}
+	}
+	if (CUST[flag].tool.kind == 'c') {
+		GoOut(CS1, CS1WIDTH, CS1LENGTH, CUST[flag].tool.place[1],
+				CUST[flag].tool.place[2]);
+	}
+	if (CUST[flag].tool.kind == 'e') {
+		switch (CUST[flag].tool.place[0]) {
+		case 2:
+			GoOut(CS2, CS2WIDTH, CS2LENGTH, CUST[flag].tool.place[1],
+					CUST[flag].tool.place[2]);
+			break;
+		case 3:
+			GoOut(CS3, CS3WIDTH, CS3LENGTH, CUST[flag].tool.place[1],
+					CUST[flag].tool.place[2]);
+			break;
+		default:
+			break;
+		}
+	}
+	switch (CUST[flag].level) {
+	case 't':
+		paypal = CHARGETABLE[CUST[flag].tool.place[0] - 1] * TEACHERDISCOUNT;
+		break;
+	case 's':
+		paypal = CHARGETABLE[CUST[flag].tool.place[0] - 1] * STUDENTDISCOUNT;
+		break;
+	case 'v':
+		paypal = CHARGETABLE[CUST[flag].tool.place[0] - 1];
+		break;
+	}
+	cout << "The Charge is $" << paypal << "!" << endl;
+	system("pause");
+}
+
+void GuestOut() {
+
+}
+
+/*
+ * Exit Desk Mod End
+ */
+
+/*
+ * Basic Function Start
+ */
 bool isFull(Slot *p, int mx, int my) {
 	for (int i = 0; i < mx * my; i++)
 		if (p[i].ispark == 0)
@@ -119,7 +288,7 @@ void RandomSlot(Slot *p, int mx, int my, int & outx, int & outy) {
 	outy = y;
 }
 
-int ParkIn(Object temp, Slot *p, int mx, int my) {
+int ParkIn(Object &temp, Slot *p, int mx, int my) {
 	int x, y;
 	if (isFull(p, mx, my))
 		return -1; //Error Code -1 Parking Area Full
@@ -138,8 +307,13 @@ int ParkIn(Object temp, Slot *p, int mx, int my) {
 
 int GoOut(Slot *p, int mx, int my, int x, int y) {
 	p[my * x + y].ispark = 0;
+	carnum--;
 	return 1;
 }
+
+/*
+ * Basic Function End
+ */
 
 void Welcome() {
 	char c;
@@ -161,6 +335,7 @@ void Welcome() {
 			EnterLogin();
 			break;
 		case '3':
+			ExitLogin();
 			break;
 		case 'q':
 			return;
@@ -174,6 +349,9 @@ void Welcome() {
 	}
 }
 
+/*
+ * Admin Mod Start
+ */
 void AdminLogin() {
 	string temp;
 	for (int i = 0; i < 3; i++) {
@@ -302,6 +480,9 @@ void SetChargeTable() {
 	printf("Please input the Discount of the Teacher: ");
 	setbuf(stdin, NULL);
 	scanf("%lf", &TEACHERDISCOUNT);
+	printf("Please input the Discount of the Student: ");
+	setbuf(stdin, NULL);
+	scanf("%lf", &STUDENTDISCOUNT);
 }
 
 void CreateAnAccount() {
@@ -345,7 +526,7 @@ void CreateAnAccount() {
 	temp.level = level;
 	temp.address = address;
 	SetObject(tool);
-	temp.tool=tool;
+	temp.tool = tool;
 	tool.cust = &temp;
 	CUST[custnum] = temp;
 	system("pause");
@@ -380,15 +561,15 @@ void SetObject(Object & obj) {
 	obj.license = licence;
 }
 
+/*
+ * Admin Mod End
+ */
 
+/*
+ * Enterance Desk Mod Start
+ */
 
-
-
-
-
-
-
-void EnterLogin(){
+void EnterLogin() {
 	string temp;
 	for (int i = 0; i < 3; i++) {
 		system("cls");
@@ -410,7 +591,7 @@ void EnterLogin(){
 	return;
 }
 
-void Enter(){
+void Enter() {
 	char c;
 	while (1) {
 		system("cls");
@@ -422,9 +603,11 @@ void Enter(){
 		setbuf(stdin, NULL);
 		scanf("%c", &c);
 		switch (c) {
-		case '1':ObjectIn();
+		case '1':
+			ObjectIn();
 			break;
-		case 'p':ChangeEntrancePasswd();
+		case 'p':
+			ChangeEntrancePasswd();
 			break;
 		case 'q':
 			return;
@@ -439,17 +622,17 @@ void Enter(){
 	system("pause");
 }
 
-void ObjectIn(){
-	char c;
-	while(1){
+void ObjectIn() {
+	char c = 0;
+	while (c != 'a' && c != 'b') {
 		system("cls");
-		setbuf(stdin, NULL);
 		printf("\t\t---Way to Enter---\n");
 		printf("\ta. Account\n");
 		printf("\tb. Guest\n");
 		printf("Please Choose: ");
-		scanf("%c",&c);
-		switch(c){
+		setbuf(stdin, NULL);
+		scanf("%c", &c);
+		switch (c) {
 		case 'a':
 			AccountIn();
 			return;
@@ -458,91 +641,127 @@ void ObjectIn(){
 			GuestIn();
 			return;
 			break;
-		default:printf("\nError!");system("pause");break;
+		default:
+			printf("\nError!");
+			system("pause");
+			break;
 		}
 	}
 }
 
-void AccountIn(){
+void AccountIn() {
 	string acc;
-	int flag=-1;
-	while(flag==-1){
+	int flag = -1;
+	while (flag == -1) {
 		system("cls");
 		setbuf(stdin, NULL);
 		printf("Please Enter Your Account Name:");
-		cin>>acc;
-		for(int i=0;i<custnum;i++){
-			if(acc==CUST[i].name){
-				flag=i;
+		cin >> acc;
+		for (int i = 0; i < custnum; i++) {
+			if (acc == CUST[i].name) {
+				flag = i;
 				break;
 			}
 		}
-		if(flag==-1){
+		if (flag == -1) {
+			setbuf(stdin, NULL);
 			printf("Account Not Found, Please reEnter");
 			system("pause");
 		}
 	}
-	if(CUST[flag].tool.kind=='c'){
-		ParkIn(CUST[flag].tool,CS1,CS1LENGTH,CS1WIDTH);
+	if (CUST[flag].tool.kind == 'c') {
+		CUST[flag].tool.place[0] = 1;
+		ParkIn(CUST[flag].tool, CS1, CS1LENGTH, CS1WIDTH);
 		printf("Parkin Success!");
 	}
-	if(CUST[flag].tool.kind=='e'){
+	if (CUST[flag].tool.kind == 'e') {
 		char c;
-		while(c!='2'&&c!='3'){
+		while (c != '2' && c != '3') {
 			system("cls");
 			setbuf(stdin, NULL);
 			printf("Please Choose Which Parking Space(2 for CS2, 3 for CS3):");
-			scanf("%c",&c);
-			switch(c){
-			case'2':ParkIn(CUST[flag].tool,CS2,CS2LENGTH,CS2WIDTH);break;
-			case'3':ParkIn(CUST[flag].tool,CS3,CS3LENGTH,CS3WIDTH);break;
-			default:printf("\nError!");system("pause");break;
+			scanf("%c", &c);
+			switch (c) {
+			case '2':
+				CUST[flag].tool.place[0] = 2;
+				ParkIn(CUST[flag].tool, CS2, CS2LENGTH, CS2WIDTH);
+				break;
+			case '3':
+				CUST[flag].tool.place[0] = 3;
+				ParkIn(CUST[flag].tool, CS3, CS3LENGTH, CS3WIDTH);
+				break;
+			default:
+				printf("\nError!");
+				system("pause");
+				break;
 			}
 		}
-		printf("Parking Success!");
-		system("pause");
 	}
+	setbuf(stdin, NULL);
+	cout << CUST[flag].tool.place[1] << " " << CUST[flag].tool.place[2] << endl;
+	printf("Parking Success!");
+	system("pause");
 }
 
-void GuestIn(){
-	bool flag=1;
+void GuestIn() {
+	bool flag = 1;
 	char c;
 	string lic;
 	Object temp;
-	while(flag){
+	while (flag) {
 		system("cls");
 		setbuf(stdin, NULL);
 		printf("Please Enter the Vehicle type(c for Car, e for eBike):");
-		scanf("%c",&c);
-		switch(c){
-		case 'c':temp.kind=c;flag=0;break;
-		case 'e':temp.kind=c;flag=0;break;
-		default:printf("\nError!");system("pause");break;
+		scanf("%c", &c);
+		switch (c) {
+		case 'c':
+			temp.kind = c;
+			flag = 0;
+			break;
+		case 'e':
+			temp.kind = c;
+			flag = 0;
+			break;
+		default:
+			printf("\nError!");
+			system("pause");
+			break;
 		}
 	}
 	system("cls");
 	setbuf(stdin, NULL);
 	printf("Please Enter The Licence: ");
-	cin>>lic;
-	temp.license=lic;
-	temp.cust=NULL;
-	if(temp.kind=='c'){
-		ParkIn(temp,CS1,CS1LENGTH,CS1WIDTH);
-	}
-	else{
+	cin >> lic;
+	temp.license = lic;
+	temp.cust = NULL;
+	if (temp.kind == 'c') {
+		temp.place[0] = 1;
+		ParkIn(temp, CS1, CS1LENGTH, CS1WIDTH);
+	} else {
 		char c;
-		while(1){
+		while (c != '2' && c != '3') {
 			system("cls");
 			setbuf(stdin, NULL);
 			printf("Please Choose Which Parking Space(2 for CS2, 3 for CS3):");
-			scanf("%c",&c);
-			switch(c){
-			case'2':ParkIn(temp,CS2,CS2LENGTH,CS2WIDTH);break;
-			case'3':ParkIn(temp,CS3,CS3LENGTH,CS3WIDTH);break;
-			default:printf("\nError!");system("pause");break;
+			scanf("%c", &c);
+			switch (c) {
+			case '2':
+				temp.place[0] = 2;
+				ParkIn(temp, CS2, CS2LENGTH, CS2WIDTH);
+				break;
+			case '3':
+				temp.place[0] = 3;
+				ParkIn(temp, CS3, CS3LENGTH, CS3WIDTH);
+				break;
+			default:
+				printf("\nError!");
+				system("pause");
+				break;
 			}
 		}
 	}
+	printf("Parking Success!");
+	system("pause");
 }
 
 void ChangeEntrancePasswd() {
@@ -561,6 +780,10 @@ void ChangeEntrancePasswd() {
 	}
 	return;
 }
+
+/*
+ * Enterance Desk Mod End
+ */
 
 int main() {
 	Welcome();
