c947c717c248c009d693fbc403b7fb09e45bd488
wddddw <im.wd95@gmail.com>
2014-12-06 11:47:07 +0800

    Relese Version 1.0.2
    Fix Some Format Problem

--- cparking.vol1.cpp
+++ cparking.vol1.cpp
@@ -12,7 +12,14 @@
 #include <ctime>
 using namespace std;
 #define MAXSIZE 8
-//Max width of a car license#define MAXCAR 2150#define MAXCUST 10000struct Customer;struct Object {	Customer * cust;
+
+//Max width of a car LICENSE
+
+#define MAXCAR 2150
+#define MAXCUST 10000
+struct Customer;
+struct Object {
+	Customer * cust;
 	char kind; //Recording the kind of the object
 	string license; //Recording the License of the Car or eBike
 	int place[3]; //[0] to Record CS[Where], [1] to Record x, [2] to Record y
